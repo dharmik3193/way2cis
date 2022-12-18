b5383e085084752b8a9f6b2a1624546f64c5cfd4
dharmik3193 <dnbambharoliya555@gmail.com>
2022-12-18 08:20:47 +0530

    way2cis

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import Features from "../Components/Features";
 import Header from "../Components/Header"
 import Slider from "../Components/Slider";
+import Sliderdemo from "../Components/Sliderdemo";
 
 const Home = () => {
      return (
           <>
                <Header />
-               <Slider />
+               {/* <Slider /> */}
+               <Sliderdemo />
                <Features />
           </>
      )
