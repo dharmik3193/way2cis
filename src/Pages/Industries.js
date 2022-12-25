ef31e1d4bb08dadce2059a34f3329222b22e26e8
dharmik3193 <dnbambharoliya555@gmail.com>
2022-12-25 20:46:14 +0530

    Way2Cis

---
+++ src/Pages/Industries.js
@@ -0,0 +1,7 @@
+const Industries = () => {
+     return (
+          <h1>This Is industries Page</h1>
+     )
+}
+
+export default Industries;
---
+++ src/Pages/Product.js
@@ -0,0 +1,8 @@
+const Products = () => {
+     <>
+          <h1>This Is Product Page</h1>
+     </>
+     
+}
+
+export default Products;
